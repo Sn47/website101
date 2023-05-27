edbd5851df0a3a95c9e8400fe30e3ddd40401f95
AhmadShykh <52326090+AhmadShykh@users.noreply.github.com>
2023-05-27 22:57:57 +0500

    changed mail

--- forgotPassword.php
+++ forgotPassword.php
@@ -29,10 +29,10 @@ include ("connection.php");
                         
                     }
                     else{
-                        $transport = Transport::fromDsn('smtp://ahmadshykh2015@gmail.com:ikknbqpjpdphtlhz@smtp.gmail.com:587');
+                        $transport = Transport::fromDsn('smtp://senderemail@gmail.com:twostepverfiserver@smtp.gmail.com:587');
                         $mailer = new Mailer($transport); 
                         $email = (new Email());
-                        $email->from('ahmadshykh2015@gmail.com');
+                        $email->from('senderemail@gmail.com');
                         $email->to($emailPost);
                         $email->subject('Password Recovery');
                         $email->html('<p>The token for your password recovery is <b>'.$token.'</b><br>Remember the code will retire in 5 minutes</p>');
@@ -83,26 +83,26 @@ include ("connection.php");
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dosis">
-        <link rel="stylesheet" href="./signupfrom.css?v=<?php echo time(); ?>">
-        <title>Forgot Password</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dosis">
+    <link rel="stylesheet" href="./signupfrom.css?v=<?php echo time(); ?>">
+    <title>Forgot Password</title>
+</head>
 
 
-    <body style="display: flex;align-items:center">
-        <div class="fpass-div">
-            <h2 class="" style="text-align:center">Forgot Password</h2>
-            <br>
-            <form action="" class="fp-form" id="myform" method="POST" enctype="multipart/form-data ">
-                <div style="display: grid">
-                    <label class="forPassLabl" for="email">Enter Email</label>
-                    <input class="forPassInp" type="email" id="email" name="email" autocomplete="off">
-                    <div id="emailError" class="error"></div>
-                    <?php 
+<body style="display: flex;align-items:center">
+    <div class="fpass-div">
+        <h2 class="" style="text-align:center">Forgot Password</h2>
+        <br>
+        <form action="" class="fp-form" id="myform" method="POST" enctype="multipart/form-data ">
+            <div style="display: grid">
+                <label class="forPassLabl" for="email">Enter Email</label>
+                <input class="forPassInp" type="email" id="email" name="email" autocomplete="off">
+                <div id="emailError" class="error"></div>
+                <?php 
                         if (!empty($msg)) 
                             echo /*html */'<div class = "error" style = "font-size:16px;color:rgb(255,0,0)">'.$msg.'</div>' ;
                         if(!empty($succMsg)){
@@ -110,43 +110,43 @@ include ("connection.php");
                         }
                     ?>
 
-                    <button name="fgPass" id="fpassbutton" type="submit" form="myform">
-                        Submit Email
-                    </button>
-                </div>
+                <button name="fgPass" id="fpassbutton" type="submit" form="myform">
+                    Submit Email
+                </button>
+            </div>
 
-                <br>
-                <br>
-                <div style="display: grid">
-                    <label class="forPassLabl" for="code">Enter Code</label>
-                    <input class="forPassInp" type="text" id="code" name="code" autocomplete="off">
-                    <?php 
+            <br>
+            <br>
+            <div style="display: grid">
+                <label class="forPassLabl" for="code">Enter Code</label>
+                <input class="forPassInp" type="text" id="code" name="code" autocomplete="off">
+                <?php 
                             if (!empty($cdErrMsg)) 
                                 echo /*html */'<div class = "error" style = "font-size:16px;color:rgb(255,0,0)">'.$cdErrMsg.'</div>' ;
                             
                         ?>
-                    <button name="codeButt" id="codeButt" type="submit" form="myform">
-                        Submit Code
-                    </button>
-                </div>
+                <button name="codeButt" id="codeButt" type="submit" form="myform">
+                    Submit Code
+                </button>
+            </div>
 
 
-            </form>
+        </form>
 
-        </div>
-    </body>
+    </div>
+</body>
 
-    <script>
-    const email = document.querySelector('#email');
-    const emailError = document.querySelector('#emailError');
-    email.addEventListener("input", function(event) {
-        eErr.textContent = '';
-        if (email.validity.typeMismatch) {
-            emailError.textContent = 'Please enter in a valid Email. ex(johnSmith@email.com)';
-        } else {
-            emailError.textContent = '';
-        }
-    });
-    </script>
+<script>
+const email = document.querySelector('#email');
+const emailError = document.querySelector('#emailError');
+email.addEventListener("input", function(event) {
+    eErr.textContent = '';
+    if (email.validity.typeMismatch) {
+        emailError.textContent = 'Please enter in a valid Email. ex(johnSmith@email.com)';
+    } else {
+        emailError.textContent = '';
+    }
+});
+</script>
 
 </html>
